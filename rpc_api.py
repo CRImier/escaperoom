e30812b776b2a83853bb8bfc2bb482095fe83885
CRImier <crimier@yandex.ru>
2016-10-25 01:28:33 +0300

    Added RPC API threaded mode

--- rpc_api.py
+++ rpc_api.py
@@ -1,8 +1,15 @@
+from SocketServer import ThreadingMixIn
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 import logging
 
+import threading
+
+class SimpleThreadedJSONRPCServer(ThreadingMixIn, SimpleJSONRPCServer):
+    pass
+
 class RPCApi():
     server = None
+
     def __init__(self):
         pass
 
@@ -10,7 +17,7 @@ class RPCApi():
         self.config = config
         self.objects = kwargs #We pass object_name:object pairs with all the objects whose functions have to be registered.
         if not self.server:
-            self.server = SimpleJSONRPCServer((self.config['host'], self.config['port']))
+            self.server = SimpleThreadedJSONRPCServer((self.config['host'], self.config['port']))
         #Doesn't support changing server's host/port on the fly, but can be easily added if needed
         self.server.timeout = self.config['timeout']
         #TODO: unregister all the functions maybe? Is there even such an action? Would it be necessary?
@@ -28,3 +35,17 @@ class RPCApi():
     def poll(self):
         logging.debug("RPC API: polling...")
         self.server.handle_request() #TODO: remake so that it fullfills all the requests available?
+
+    def run(self):
+        while not self.stop_thread:
+            self.server.serve_forever()
+
+    def start_thread(self):
+        logging.debug("RPC API: running...")
+        self.stop_thread = False
+        self.thread = threading.Thread(target = self.run, args = ())
+        self.thread.daemon = True
+        self.thread.start()
+
+    def stop_thread(self):
+        self.stop_thread = True
